d066bdeaa4a1f9a0d9649d8af62ece63094e82ce
Tyson <tysondavidparker@gmail.com>
2026-01-21 22:13:01 -0700

    Gang! I did it, I got the bishop to move diagonally, now I just gotta figure out enemy pieces

--- shared/src/main/java/chess/Bishopmovecalc.java
+++ shared/src/main/java/chess/Bishopmovecalc.java
@@ -8,16 +8,18 @@ public class Bishopmovecalc implements PieceMovesCalculator {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
         Collection<ChessMove> moves = new ArrayList<>();
-        for (int i = 1; i < 4; i++) {
+        int[][] increments = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
 
+        for (int i = 0; i < 4; i++) {
+            int[] curpos = {position.getRow(), position.getColumn()};
+            int[] newpos = {position.getRow() + increments[i][0], position.getColumn() + increments[i][1]};
 
-        }
-        while (position.getColumn() > 8 || position.getColumn() < 1 || position.getRow() < 1 || position.getRow() > 8) {
-
-            if (position.getColumn() > 8 || position.getColumn() < 1 || position.getRow() < 1 || position.getRow() > 8) {
-                break;
+            while (newpos[0] >= 1 && newpos[0] <= 8 && newpos[1] >= 1 && newpos[1] <= 8) {
+                moves.add(new ChessMove(new ChessPosition(curpos[0], curpos[1]), new ChessPosition(newpos[0], newpos[1]), null));
+                newpos[0] += increments[i][0];
+                newpos[1] += increments[i][1];
             }
         }
+        return moves;
     }
-
 }
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -3,6 +3,7 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -18,6 +19,20 @@ public class ChessPiece {
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
         this.pieceColor = pieceColor;
         this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 
     /**
