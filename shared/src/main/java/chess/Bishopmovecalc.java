854de3a4d38ccf3168dd2ad6610c6673d73f296a
Tyson <tysondavidparker@gmail.com>
2026-01-21 22:28:10 -0700

    It's offical, I got all the Bishop moves done!

--- shared/src/main/java/chess/Bishopmovecalc.java
+++ shared/src/main/java/chess/Bishopmovecalc.java
@@ -2,7 +2,6 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
 public class Bishopmovecalc implements PieceMovesCalculator {
     @Override
@@ -15,9 +14,16 @@ public class Bishopmovecalc implements PieceMovesCalculator {
             int[] newpos = {position.getRow() + increments[i][0], position.getColumn() + increments[i][1]};
 
             while (newpos[0] >= 1 && newpos[0] <= 8 && newpos[1] >= 1 && newpos[1] <= 8) {
-                moves.add(new ChessMove(new ChessPosition(curpos[0], curpos[1]), new ChessPosition(newpos[0], newpos[1]), null));
-                newpos[0] += increments[i][0];
-                newpos[1] += increments[i][1];
+                if(board.getPiece(new ChessPosition(newpos[0],newpos[1])) == null || board.getPiece(new ChessPosition(newpos[0],newpos[1])).getTeamColor() != board.getPiece(position).getTeamColor()) {
+                    if (board.getPiece(new ChessPosition(newpos[0],newpos[1])) != null) {
+                        moves.add(new ChessMove(new ChessPosition(curpos[0], curpos[1]), new ChessPosition(newpos[0], newpos[1]), null));
+                        break;
+                    }
+                    moves.add(new ChessMove(new ChessPosition(curpos[0], curpos[1]), new ChessPosition(newpos[0], newpos[1]), null));
+                    newpos[0] += increments[i][0];
+                    newpos[1] += increments[i][1];
+                }
+                else {break;}
             }
         }
         return moves;
