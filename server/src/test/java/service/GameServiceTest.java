2e2e18cf9c20e936b2f8794d9061477f712795cb
Tyson <tysondavidparker@gmail.com>
2026-03-02 23:53:26 -0700

    Create game tests

--- server/src/test/java/service/GameServiceTest.java
+++ server/src/test/java/service/GameServiceTest.java
@@ -1,4 +1,37 @@
 package service;
 
+import dataaccess.MemoryDataAccess;
+import dataaccess.exception.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.RequestAndResult.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GameServiceTest {
+    private GameService service;
+
+    @BeforeEach
+    void setup() {
+        service = new GameService(new MemoryDataAccess());
+    }
+
+//    @Test
+//    void createGamePositive() throws Exception {
+//        CreateGameRequest request = new CreateGameRequest("CoolGame");
+//
+//
+//
+//        assertNotNull(result);
+//        assertTrue(result.gameID() > 0);
+//    }
+
+    @Test
+    void createGameUnauthorized() {
+
+        CreateGameRequest request = new CreateGameRequest("CoolGame");
+
+        assertThrows(UnauthorizedException.class, () -> service.createGame(request, "badToken"));
+    }
+
 }
